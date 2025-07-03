cfef23bc31be71aaf1d67cc907cb1a8464d48c2f
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-07-03 21:52:44 +0200

    build even older nightlies

--- src/nightlies.rs
+++ src/nightlies.rs
@@ -9,7 +9,7 @@ use tracing::debug;
 
 use crate::db::{BuildMode, FinishedNightly};
 
-const EARLIEST_CUTOFF_DATE: &str = "2023-01-01";
+const EARLIEST_CUTOFF_DATE: &str = "2022-01-01";
 
 #[derive(Default)]
 pub struct NightlyCache {
