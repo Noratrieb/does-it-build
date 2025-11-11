73098095a71b989311fb96691ff8850ad58f5d56
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2025-11-11 19:03:37 +0100

    Optimize features

--- src/web.rs
+++ src/web.rs
@@ -101,14 +101,10 @@ async fn web_build(State(state): State<AppState>, Query(query): Query<BuildQuery
                 rustflags: build.rustflags,
                 version: crate::VERSION,
                 status: build.status,
-                build_date: build.build_date.map(|build_date| {
-                    time::OffsetDateTime::from_unix_timestamp_nanos(build_date as i128 * 1000000)
-                        .map(|build_date| {
-                            build_date
-                                .format(&time::format_description::well_known::Rfc3339)
-                                .unwrap()
-                        })
-                        .unwrap()
+                build_date: build.build_date.and_then(|build_date| {
+                    jiff::Timestamp::from_millisecond(build_date)
+                        .ok()
+                        .map(|build_date| build_date.to_string())
                 }),
                 build_duration_s: build
                     .build_duration_ms
